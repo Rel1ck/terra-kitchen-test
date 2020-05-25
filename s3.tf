2ef6e8015aba26253c04052ca85795e3ce9c4286
Leomar <leomar.moncada@naranja.com>
2020-05-25 18:54:25 -0300

    owner added

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_txt_bucket" {
-  bucket = "${var.environment}-leomar-test-bucket"
+  bucket = "${var.environment}-${var.owner}-test-bucket"
   acl    = "private"
 
   tags = {
